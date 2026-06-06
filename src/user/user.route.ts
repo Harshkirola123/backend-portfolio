19cb9aa15f121476931b405905354f06b8e68191
Harshit <harshitkirola123@gmail.com>
2026-06-06 10:05:58 +0530

    Add middlerware check in logout route

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -8,7 +8,7 @@ router.post("/", userController.createUser);
 router.post("/login", userController.login);
 
 router.post("/refresh-token", userController.refreshToken);
-router.post("/logout", userController.logout);
+router.post("/logout", authMiddleware, userController.logout);
 router.get("/", authMiddleware, userController.getUserDetail);
 
 export default router;
